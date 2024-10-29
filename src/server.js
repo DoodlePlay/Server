c80391779d96e6fe2469ec1ebc614059f2cc1133
sumin-dev <bonar8888@naver.com>
2024-10-30 08:01:44 +0900

    [FIX] ToxicCover 유저간 동일 위치로 수정 #16

--- src/server.js
+++ src/server.js
@@ -306,6 +306,38 @@ io.on('connection', (socket) => {
     gameState.items[itemId].user = socket.id;
     gameState.items[itemId].status = true;
 
+    if (itemId === 'toxicCover') {
+      const toxicImagePaths = [
+        '/images/drawing/items/effects/toxic01.svg',
+        '/images/drawing/items/effects/toxic02.svg',
+        '/images/drawing/items/effects/toxic03.svg',
+        '/images/drawing/items/effects/toxic04.svg',
+        '/images/drawing/items/effects/toxic05.svg',
+        '/images/drawing/items/effects/toxic06.svg',
+        '/images/drawing/items/effects/toxic07.svg',
+      ];
+
+      const step = Math.floor(80 / Math.sqrt(7));
+      let leftPos = 0;
+      let topPos = 0;
+
+      const toxicPositions = Array.from({ length: 7 }, (_, i) => {
+        const src = toxicImagePaths[i % toxicImagePaths.length];
+        const left = leftPos + Math.random() * step;
+        const top = topPos + Math.random() * step;
+
+        leftPos += step;
+        if (leftPos >= 80) {
+          leftPos = 0;
+          topPos += step;
+        }
+
+        return { src, left, top };
+      });
+
+      io.to(roomId).emit('toxicEffectPositions', toxicPositions);
+    }
+
     io.to(roomId).emit('gameStateUpdate', gameState);
   });
 
